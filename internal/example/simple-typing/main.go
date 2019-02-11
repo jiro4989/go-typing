2271113201fd35f2b6c3fc2f9d806578463841f9
jiro4989 <jiroron666@gmail.com>
2019-02-11 20:57:23 +0900

    :tada:init commit

---
+++ internal/example/simple-typing/main.go
@@ -0,0 +1,46 @@
+package main
+
+import (
+	termbox "github.com/nsf/termbox-go"
+)
+
+func main() {
+	if err := termbox.Init(); err != nil {
+		panic(err)
+	}
+	defer termbox.Close()
+
+	termbox.SetInputMode(termbox.InputEsc)
+	termbox.Flush()
+
+	text := "golang"
+
+	for {
+		setText(text)
+		if len(text) < 1 {
+			return
+		}
+		switch ev := termbox.PollEvent(); ev.Type {
+		case termbox.EventKey:
+			switch ev.Key {
+			case termbox.KeyCtrlC, termbox.KeyCtrlD:
+				return
+			}
+			ch := ev.Ch
+			top := rune(text[0])
+			same := ch == top
+			if same {
+				text = text[1:]
+			}
+		}
+	}
+}
+
+func setText(text string) {
+	dc := termbox.ColorDefault
+	termbox.Clear(dc, dc)
+	for i, ch := range text {
+		termbox.SetCell(1+i, 1, ch, dc, dc)
+	}
+	termbox.Flush()
+}
